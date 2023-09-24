e47da7221283aaad6e1a04dc33019422110de318
DjhonyMaga <djonatandeoliveira@hotmail.com>
2023-09-23 21:16:05 -0300

    functions

--- 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
+++ 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
@@ -7,20 +7,37 @@ fullStackPHPClassName("02.11 - Trabalhando com funções");
  */
 fullStackPHPClassSession("functions", __LINE__);
 
+require __DIR__ . "/functions.php";
+
+var_dump(functionName("Djoantan", "João", "Sergio"));
+
+var_dump(optionsArg("Djoantan"));
+var_dump(optionsArg("Djoantan", "João"));
+var_dump(optionsArg("Djoantan", "João", "Matheus"));
 
 /*
  * [ global access ] global $var
  */
 fullStackPHPClassSession("global access", __LINE__);
 
+$weight = 86;
+$height = 1.86;
+echo calcImc();
 
 /*
  * [ static arguments ] static $var
  */
 fullStackPHPClassSession("static arguments", __LINE__);
 
+$pay = payTotal(200);
+$pay = payTotal(150);
+$pay = payTotal(150);
+
+echo $pay;
 
 /*
  * [ dinamic arguments ] get_args | num_args
  */
 fullStackPHPClassSession("dinamic arguments", __LINE__);
+
+var_dump(myTeam("Djonatan", "João", "Caio"));
